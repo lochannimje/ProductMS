ac2cb6eb47a3b849a411cfeaa49a3b7a9c6d9bd6
lochannimje <65588981+lochannimje@users.noreply.github.com>
2020-06-15 17:26:12 +0530

    Update ProductApplication.java

--- src/main/java/team12/product/ProductApplication.java
+++ src/main/java/team12/product/ProductApplication.java
@@ -5,7 +5,6 @@ package team12.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +12,6 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan
-@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
